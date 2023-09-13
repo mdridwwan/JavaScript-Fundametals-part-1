d4030d0d56cb0e53c3eee3ad3f4a9e85f22d9e37
mdridwwan <mdridwan583011@gmail.com>
2023-09-13 15:58:53 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+      60+20+13-20
+      console.log(60+20+13-20)
+
+      //camelCase//
+let firstName = "Ridwan";
+
+console.log(firstName);
+
+
+let first_name_person;
+let firstNamePerson;
+
+// let 3years = 3;
+
+let farhan_ahmed_ridwan = "Teacher";
+let PI = 3.444;
+
+let myFristJob ="Programmer";
+let myCurrentJob ="Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFristJob);
+console.log(myCurrentJob);
+
+console.log(job1);
+console.log(job2);
