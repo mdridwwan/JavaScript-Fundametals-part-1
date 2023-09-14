d024ade9c5c335845c72c67c990e8ba14bb4708f
mdridwwan <mdridwan583011@gmail.com>
2023-09-14 19:41:24 +0600

    last Update

--- script.js
+++ script.js
@@ -1,30 +1,131 @@
 
 
-      60+20+13-20
-      console.log(60+20+13-20)
+//       60+20+13-20
+//       console.log(60+20+13-20)
 
-      //camelCase//
-let firstName = "Ridwan";
+//       //camelCase//
+// let firstName = "Ridwan";
 
-console.log(firstName);
+// console.log(firstName);
 
 
-let first_name_person;
-let firstNamePerson;
+// let first_name_person;
+// let firstNamePerson;
 
-// let 3years = 3;
+// // let 3years = 3;
 
-let farhan_ahmed_ridwan = "Teacher";
-let PI = 3.444;
+// let farhan_ahmed_ridwan = "Teacher";
+// let PI = 3.444;
 
-let myFristJob ="Programmer";
-let myCurrentJob ="Teacher";
+// let myFristJob ="Programmer";
+// let myCurrentJob ="Teacher";
 
-let job1 = "Programmer";
-let job2 = "Teacher";
+// let job1 = "Programmer";
+// let job2 = "Teacher";
 
-console.log(myFristJob);
-console.log(myCurrentJob);
+// console.log(myFristJob);
+// console.log(myCurrentJob);
 
-console.log(job1);
-console.log(job2);
+// console.log(job1);
+// console.log(job2);
+
+
+// var object = {
+//       data:"10",
+//       roll: "01",
+//       name: "Ridwan", 
+// }
+
+
+// var roll = 10;
+// var name = "Ridwan";
+// var iAmGoodBoy = true;
+// var dhaka = null;
+// var mango;
+// //Symbol//
+
+// console.log(typeof(roll))
+// console.log(typeof(name))
+// console.log(typeof(iAmGoodBoy))
+// console.log(typeof(dhaka))
+// console.log(typeof(mango))
+
+
+// const now = 2037;
+// const ridwan = now - 2003;
+// const halima = now - 1998;
+
+// console.log(ridwan, halima);
+// console.log(ridwan * 2, ridwan / 2, 2 ** 4) // 2+2=4*2=8*2=16
+
+// // Assign Operatorts
+// let x = 10+5; //15
+// x += 10; //25
+// x *= 4; //100
+// x++ // x+1;
+// x-- // x-1;
+
+// // Comparison Operators
+// console.log(30 > 20) // >, <, >=, <=. 
+// console.log(10 <= 10)
+
+
+// Precedence Operators
+
+// const now = 2037;
+// const ridwan = now - 2003;
+// const halima = now - 1998;
+
+// console.log(now - 2003 > now - 1998) // false
+
+// let a,b;
+// a = b = 25 - 10 - 5;
+// console.log(a,b); // 10, 10;
+
+//Challang 1.
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const messJhon = 92;
+// const heightJhon = 1.95;
+
+// const BMIMark = massMark / heightMark ** 2 ;
+// const BMIJhon = messJhon / (heightJhon * heightJhon);
+// const markHigherBMI = BMIMark > BMIJhon;
+// console.log(BMIMark, BMIJhon, markHigherBMI);
+
+// Strind Template.
+
+// const firstName = "Ridwan";
+// const job = "Teacher";
+// const birthYear = 2003;
+// const year = 2037;
+
+// const ridwan = "I'm " + firstName + ', a ' + (year - birthYear) + ' Years old ' + job + '!';
+
+// console.log(ridwan)
+
+// const ridwanNew = `I'm ${firstName}, a ${year - birthYear}, Years old ${job} !`;
+
+// console.log(ridwan);
+// console.log(`Just a regular string....`);
+// console.log('String with \n\ multiple \n\ line');
+
+
+
+// What is variable? 
+
+// var student ="Ridwan";
+//  let student ="Ridwan";
+const adding = 2 + 2;
+const division = 10 / 2; // constant
+// student = "Rony";
+
+console.log(adding);
+console.log(division);
+
+// var fun = function (){
+
+//     console.log(student);
+// }
+
